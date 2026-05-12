16f1add96426a68094bdcb6dd0d17de1856174e7
Asan Indusara <asanindusara348@gmail.com>
2026-05-13 00:03:21 +0530

    fix gmail link for mobile and desktop and fixes for better mobile view

--- script.js
+++ script.js
@@ -6,3 +6,22 @@
   html.setAttribute('data-bs-theme', isLight ? 'dark' : 'light');
   btn.innerText = isLight ? 'Light' : 'Dark';
   });
+
+const gmailLink = document.getElementById('gmail-link');
+const email = "asanindusara348@gmail.com";
+
+// Desktop Gmail URL (Opens in browser tab)
+const desktopGmail = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}`;
+
+// Mobile Link (Triggers Gmail App)
+const mobileMailto = `mailto:${email}`;
+
+// Check if the user is on a mobile device
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+// Set the correct link based on the device
+if (isMobile) {
+    gmailLink.href = mobileMailto;
+} else {
+    gmailLink.href = desktopGmail;
+}
